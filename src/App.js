efcf4b4ea242a37e06eb17afec88be853b7748d0
ida.pousse <idapousse@yahoo.fr>
2019-10-09 17:34:44 +0200

    Fetch from API

--- src/App.js
+++ src/App.js
@@ -1,38 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+import { CardList } from './components/card-list/card-list.component';
+
 import './App.css';
 
 class App extends Component {
-
   constructor() {
     super();
 
     this.state = {
-      settings : [
-       {name: 'Beach'},
-       {name: 'Greenery'},
-       {name: 'Flowers'},
-       {name: 'Clouds'},
-       {name: 'Wildlife'}
-      ]
+      landscapes: []
     };
   }
 
+  //  Life Cycle method (we have access to it because of our class component)
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ landscapes: users }));
+  } /* 1st we've made a native JavaScript fetch request in order to fetch and make an api request 
+  to a specific url, that fetch request returns a response (promise), which we demand that it be 
+  converted into a json format, then we're taking the 'users' list that we got back from that file 
+  and we set our landscapes array to a new state (via this.setState) which puts the users list in 
+  our array of landscapes (which we emptied for that purpose)*/
+
   render() { 
     return ( 
-      <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {this.state.string}
-        </p>
-        <button onClick={() => this.setState({ string: 'Hello People!'})}>
-          Change Text
-        </button>
-      </header>
+    <div className='App'>
+      <CardList name="Sand">
+        {this.state.landscapes.map(landscape => 
+        (<h1 key={landscape.id}>{landscape.username}</h1>
+        ))} 
+      </CardList>
+      {/* <img src='http://loremflickr.com/400/300/moto' alt=''/> */}
     </div>
-
-     );
+   );
   }
 }
  
